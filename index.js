53ea755d34d72450a28b326f81c8e4a4e7835890
Joselyn Do <joselynd@uw.edu>
2024-04-26 11:43:07 -0700

    Create index.js and add basic format and header comment

---
+++ index.js
@@ -0,0 +1,18 @@
+/**
+ * Header comment for index.js
+ */
+
+"use strict";
+
+(function() {
+
+  window.addEventListener("load", init);
+
+  /**
+   * Initializes the ________
+   */
+  function init() {
+
+  }
+
+})();
