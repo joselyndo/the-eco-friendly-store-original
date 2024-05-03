f559a40a6f0772ddddf120723194e8ba39d3b250
Keith Nguyen <keith123@uw.edu>
2024-05-03 11:21:04 -0700

    Style edits

--- index.js
+++ index.js
@@ -15,4 +15,53 @@
 
   }
 
+  /**
+   * Helper function to return the response's result text if successful, otherwise
+   * returns the rejected Promise result with an error status and corresponding text
+   * @param {object} res - response to check for success/error
+   * @return {object} - valid response if response was successful, otherwise rejected
+   *                    Promise result
+   */
+  async function statusCheck(res) {
+    if (!res.ok) {
+      throw new Error(await res.text());
+    }
+    return res;
+  }
+
+  /**
+   * Returns a generated DOM object of type tag.
+   * @param {string} tag element tag
+   * @returns {object} DOM object created with tag
+   */
+  function gen(tag) {
+    return document.createElement(tag);
+  }
+
+  /**
+   * Returns the element that has the ID attribute with the specified value.
+   * @param {string} id - element ID
+   * @return {object} DOM object associated with id.
+   */
+  function id(id) {
+    return document.getElementById(id);
+  }
+
+  /**
+   * Returns the element that matches the given CSS selector.
+   * @param {string} query - CSS query selector
+   * @returns {object} DOM object matching the query.
+   */
+  function qs(query) {
+    return document.querySelector(query);
+  }
+
+  /**
+   * Returns the array of elements that match the given CSS selector.
+   * @param {string} query - CSS query selector
+   * @returns {object[]} array of DOM objects matching the query.
+   */
+  function qsa(query) {
+    return document.querySelectorAll(query);
+  }
 })();
